f791d1b332b2d8de4c814c67a3addfb787481106
matheusgomessouza <matheusg_souza@outlook.com>
2022-06-23 20:43:31 -0300

    feat:Subscription with GraphQL

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-	uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o0vqw0038t01z24h0a7s5f/master',
+	uri: import.meta.env.VITE_API_URL,
+	headers: {
+		'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+	},
 	cache: new InMemoryCache()
 });
